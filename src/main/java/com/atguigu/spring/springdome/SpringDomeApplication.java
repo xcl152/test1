315b97b7b34a66a915f7204fa94f014b13035715
xcl <2651443237@qq.com>
2022-07-20 17:53:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/spring/springdome/SpringDomeApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.spring.springdome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDomeApplication.class, args);
+        System.out.println("今天是个好日子");
+    }
+
+}
---
+++ src/test/java/com/atguigu/spring/springdome/SpringDomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.spring.springdome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
