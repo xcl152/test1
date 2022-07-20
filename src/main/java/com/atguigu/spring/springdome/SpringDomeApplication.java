140d1e86c99df857d574ffb6382d8b6700441f3c
xcl152 <106719195+xcl152@users.noreply.github.com>
2022-07-20 17:59:43 +0800

    second commit

--- src/main/java/com/atguigu/spring/springdome/SpringDomeApplication.java
+++ src/main/java/com/atguigu/spring/springdome/SpringDomeApplication.java
@@ -9,6 +9,8 @@ public class SpringDomeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDomeApplication.class, args);
         System.out.println("今天是个好日子");
+        System.out.println("今天是个好日子22");
     }
+    
 
 }
